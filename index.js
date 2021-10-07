3298c0900f528fcfcfbcb09e00a17a4ab9fa3288
Ian <imacmumf@gmail.com>
2021-10-07 14:44:15 -0500

    Fix eval spaces / add blinking answer / resize buttons

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-
 // $("document").ready(function(){
 //   $("h1").addClass();
 
 // });
 
-
 let equation = [];
-
 
 // $("button").click(function(){
 //   const audio = new Audio ("beep1.wav");
@@ -15,61 +12,62 @@ let equation = [];
 
 $(".numbers").click(function () {
   equation.push(this.value);
-  $("textarea").text(equation.join(' '));
-  const audio = new Audio ("./sounds/beep2.wav");
+  $("textarea").text(equation.join(" "));
+  const audio = new Audio("./sounds/beep2.wav");
   audio.play();
-  
 });
-
 
 $(".add").click(function () {
   equation.push(" + ");
-  $("textarea").text(equation.join(' '));
-  const audio = new Audio ("./sounds/beep1.wav");
+  $("textarea").text(equation.join(" "));
+  const audio = new Audio("./sounds/beep1.wav");
   audio.play();
-  
 });
 
 $(".subtract").click(function () {
   equation.push(" - ");
-  $("textarea").text(equation.join(' '));
-  const audio = new Audio ("./sounds/beep1.wav");
+  $("textarea").text(equation.join(" "));
+  const audio = new Audio("./sounds/beep1.wav");
   audio.play();
-  
-  
 });
 
 $(".divide").click(function () {
   equation.push(" / ");
-  $("textarea").text(equation.join(' '));
-  const audio = new Audio ("./sounds/beep3.wav");
+  $("textarea").text(equation.join(" "));
+  const audio = new Audio("./sounds/beep3.wav");
   audio.play();
-  
 });
-
 
 $(".multiply").click(function () {
   equation.push(" * ");
-  $("textarea").text(equation.join(' '));
-  const audio = new Audio ("./sounds/beep3.wav");
+  $("textarea").text(equation.join(" "));
+  const audio = new Audio("./sounds/beep3.wav");
   audio.play();
-  
 });
 
 $(".clear").click(function () {
   equation = [];
   $("textarea").text(equation);
-  const audio = new Audio ("./sounds/laser1.wav");
+  const audio = new Audio("./sounds/laser1.wav");
   audio.play();
 });
-
 
 $(".submit").click(function () {
   const loading = new Audio("./sounds/loading.wav");
   loading.play();
+  let x = eval(equation.join(""));
 
-  let x = eval(equation.join(' '));
-  $("textarea").text(x);
+  // add pulsing load class
+  $("textarea").addClass("loadPulsate");
+
+  // Remove class after set time
+  setTimeout(function(){
+    $("textarea").removeClass("loadPulsate");
+  }, 2000);
   
   
+
+  setTimeout(function () {
+    $("textarea").text(x);
+  }, 2000);
 });
